06e828dd39ec073be4c633537d11f8ef6948cbe5
Gergely Reményi <geriremenyi@users.noreply.github.com>
2020-10-11 21:59:06 +0200

    Releases 0.0.3 (#9)
    * Adding bug report
    
    * Adding feature request
    
    * Adding PR template
    
    * Adding empty workflow files (#3)
    
    * Account and Instrument SDK (#2)
    
    * Adding console example app and skd project
    
    * Adding dummy instrument candle access
    
    * Removing out stuff from versioning
    
    * Instrument and account SDK first shot
    
    * Publishing SDK as a nuget package (#4)
    
    * Removing project files from versioning
    
    * Adding ci/cd workflows and nuget properties
    
    * Adding open source docs
    
    * Fixing broken proj file name (#6)
    
    * Adding the option to select pricing components (#8)
    
    * Preparing for release v0.0.3

--- src/GeriRemenyi.Oanda.V20.Sdk/Instrument/Instrument.cs
+++ src/GeriRemenyi.Oanda.V20.Sdk/Instrument/Instrument.cs
@@ -1,9 +1,12 @@
 ﻿namespace GeriRemenyi.Oanda.V20.Sdk.Instrument
 {
     using GeriRemenyi.Oanda.V20.Client.Api;
+    using GeriRemenyi.Oanda.V20.Client.Instrument.Model;
     using GeriRemenyi.Oanda.V20.Client.Model;
     using GeriRemenyi.Oanda.V20.Sdk.Utilities;
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Threading.Tasks;
 
     public class Instrument
@@ -20,16 +23,36 @@
             _dateTimeFormat = dateTimeFormat;
         }
 
-        public async Task<CandlesResponse> GetCandles(CandlestickGranularity granularity, DateTime from, DateTime to)
+        public async Task<CandlesResponse> GetCandles(
+            CandlestickGranularity granularity, 
+            DateTime from, 
+            DateTime to, 
+            IEnumerable<PricingComponent>? pricingComponents = null
+        )
         {
             // TODO replace granularity to enum when fix is in
             return await _instrumentApi.GetInstrumentCandlesAsync(
                 instrument: _instrumentName,
-                granularity: granularity.ToString(),
+                granularity: granularity,
+                price: ResolvePricingComponents(pricingComponents),
                 from: from.ToOandaDateTime(_dateTimeFormat),
                 to: to.ToOandaDateTime(_dateTimeFormat)
             );
         }
 
+        private string ResolvePricingComponents(IEnumerable<PricingComponent>? pricingComponents)
+        {
+            if (pricingComponents == null)
+            {
+                pricingComponents = new List<PricingComponent>()
+                {
+                    PricingComponent.Bid,
+                    PricingComponent.Mid,
+                    PricingComponent.Ask
+                };
+            }
+
+            return string.Join("", pricingComponents.Select(pc => pc.ToPrice()));
+        }
     }
 }
